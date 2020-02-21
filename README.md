c45fb01033a57e322c9e81d3e207d219f6421e47
Kore Nordmann <mail@kore-nordmann.de>
2020-02-21 12:08:34 +0100

    Implemented basic PHP documentation generation script

---
+++ README.md
@@ -0,0 +1,6 @@
+# Opinionated API Doc Generator
+
+This small tool generates API documentation for PHP (and JavaScript) as
+Markdown files, so they can be properly rendered on Github and similar tools.
+The tool makes some assumptions (for example: always only one class per file)
+to make the generated documentation digestible.
---
+++ src/php.php
@@ -0,0 +1,56 @@
+# <?php
+if ($entity instanceOf \phpDocumentor\Reflection\Php\Interface_) {
+    e('`interface` ');
+} else {
+    if ($entity->isAbstract()) {
+        e('`abstract` ');
+    }
+
+    if ($entity->isFinal()) {
+        e('`final` ');
+    }
+}
+?> <?php e($entity->getName()); ?>
+
+
+Fully Qualified: [`<?php e($entity->getFqsen()); ?>`](<?php e($relativeSourceLocation); ?>)
+
+
+<?php $entity->getDocBlock() ? w($entity->getDocBlock()->getDescription()) : ''; ?>
+
+<?php if (count($properties)): ?>
+Property|Type|Default|Description
+--------|----|-------|-----------
+<?php foreach ($properties as $property): ?>
+<?php e($property->isStatic() ? '`static` ' : '')?>`<?php e($property->getName()); ?>`|`<?php e(implode('`, `', $property->getTypes())); ?>`|`<?php e($property->getDefault() ?: ''); ?>`|<?php e($property->getDocBlock() ? $property->getDocBlock()->getSummary() : ''); ?>
+
+<?php endforeach; ?>
+<?php endif; ?>
+
+<?php if (count($methods)): ?>
+## Methods
+
+<?php foreach ($methods as $method): ?>
+### <?php e($method->name) ?>
+
+
+`<?php e($method->signature); ?>`
+
+
+<?php $method->summary ? e('*' . $method->summary . '*') : ''; ?>
+
+
+<?php $method->description ? e('*' . $method->description . '*') : ''; ?>
+
+
+<?php if (count($method->arguments)): ?>
+Argument|Type|Default|Description
+--------|----|-------|-----------
+<?php foreach ($method->arguments as $argument): ?>
+`<?php e('$' . $argument->name); ?>`|`<?php e(($argument->isByReference ? '&' : '') . ($argument->isVariadic ? '…' : '') . $argument->type); ?>`|`<?php e($argument->default ?: ''); ?>`|<?php e($argument->description); ?>
+
+<?php endforeach; ?>
+<?php endif; ?>
+
+<?php endforeach; ?>
+<?php endif; ?>
